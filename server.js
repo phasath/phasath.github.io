4b4c2d4dba15c6acf10d3b598e4f852376e4a172
Sathler <raphael.sathler@fgv.br>
2018-09-26 18:13:35 -0700

    Usando npm para servir o site

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const serveStatic = require('serve-static');
+const compression = require('compression');
+const app = express();
+let dirPath = __dirname + "/dist";
+
+
+const SERVER_PORT = 5000;
+
+app.use(compression())
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(serveStatic(dirPath));
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+console.log('Listening on: ' + SERVER_PORT);
+console.log('Access here: http://localhost:' + SERVER_PORT);
+var server = app.listen(SERVER_PORT)
